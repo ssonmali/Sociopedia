4b336cb12926b2d9baaa629d8f1404f1d4840e0a
ssonmali <146985996+ssonmali@users.noreply.github.com>
2024-01-06 19:36:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,10 +19,18 @@ import Post from "./models/Post.js";
 import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
+app.use(cors(
+  {
+    origin:["https://sociopedia-zeta.vercel.app/"],
+    methods: ["POST" , "GET"],
+    credentials: true
+  }
+  ));
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
